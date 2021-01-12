6f05aad2fb0ec77b3cd15e53bedb399688833d44
mcaciolo <54399000+mcaciolo@users.noreply.github.com>
2021-01-12 16:31:04 +0100

    Add files via upload

---
+++ webgl/geoms/UPEPE/error.js
@@ -0,0 +1,13 @@
+errorData = {
+    "0Mk6CJzz93bhMSHmoVhKQW": {
+        "Error 1": "La propriété SZF_LCL ne peut pas être nulle (régle SZF_001)",
+        "Error 2": "Propriété 'Fabricant' obligatoire (régle FAB_002)"
+    },
+    "0YJh7HPwLBGgoYkYGJSuKD": {
+        "Error 3": "Propriété 'Code GMAO' obligatoire (régle GMA_001)",
+        "Error 4": "Propriété 'Fabricant' obligatoire (régle FAB_002)"
+    },
+    "0YJh7HPwLBGgoYkYGJSuKE": {
+        "Error 5": "Propriété 'Code GMAO' obligatoire (régle GMA_001)"
+    }    
+};
---
+++ webgl/geoms/Vénissieux/error.js
@@ -0,0 +1,13 @@
+errorData = {
+    "3$F8ghDvnEcf0t4g3cWpqz": {
+        "Error 1": "La propriété 'Surface' ne peut pas être nulle (régle SZF_001)",
+        "Error 2": "Propriété 'Type plancher' obligatoire (régle FAB_002)"
+    },
+    "2SS0Pj7nf5heNVQlXnd$E5": {
+        "Error 4": "Propriété 'Epaisseur isolant' obligatoire (régle FAB_002)"
+    },
+    "39TIcLK7n2EQpoUrijlVp0": {
+        "Error 4": "Propriété 'Matériaux' obligatoire (régle GMA_001)",
+        "Error 5": "Propriété 'Hauteur marche' obligatoire (régle GMA_001)"
+    }    
+};
